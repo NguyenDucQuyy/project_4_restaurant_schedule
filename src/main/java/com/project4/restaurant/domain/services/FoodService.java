d8965730ece83408c14e05c9d0ec0b284a7647a0
NguyenDucQuyy <quy.nd.2259@aptechlearning.edu.vn>
2025-06-26 16:20:34 +0700

    refactor

--- src/main/java/com/project4/restaurant/domain/services/FoodService.java
+++ src/main/java/com/project4/restaurant/domain/services/FoodService.java
@@ -1,10 +1,12 @@
 package com.project4.restaurant.domain.services;
 
+import feign.Headers;
 import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(value = "foodService", url = "${feign.restaurant-url}")
 public interface FoodService {
-    @RequestLine("PUT /project_4/restaurant/schedule/main-dish")
+    @RequestLine("POST /project_4/restaurant/schedule/main-dish")
+    @Headers("Content-Type: application/json")
     void updateMainDish();
 }
--- src/main/java/com/project4/restaurant/domain/services/StatisticService.java
+++ src/main/java/com/project4/restaurant/domain/services/StatisticService.java
@@ -1,13 +1,16 @@
 package com.project4.restaurant.domain.services;
 
+import feign.Headers;
 import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(value = "statisticService", url = "${feign.restaurant-url}")
 public interface StatisticService {
     @RequestLine("POST /project_4/restaurant/schedule/pre")
+    @Headers("Content-Type: application/json")
     void updateStatisticsPreDay();
 
     @RequestLine("POST /project_4/restaurant/schedule/current")
+    @Headers("Content-Type: application/json")
     void updateStatisticsToDay();
 }
